34775ba076add19db0a5cf84e64afe599022d382
DariusL <d.lapunas@gmail.com>
2013-09-29 18:26:45 +0300

    Finished SuperCanvas
    not really

--- Laboras21/SuperCanvas.cs
+++ Laboras21/SuperCanvas.cs
@@ -3,12 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Laboras21
 {
     class SuperCanvas : Canvas
     {
+        public void AddNode(Point node)
+        {
+            
+        }
 
+        public void AddEdge(Point start, Point end)
+        {
+
+        }
     }
 }
