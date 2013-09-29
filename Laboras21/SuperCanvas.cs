7c5402ebd06f6065ed771faf5204dd41bbf97c3f
DariusL <d.lapunas@gmail.com>
2013-09-29 18:10:25 +0300

    Added a canvas class

---
+++ Laboras21/SuperCanvas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Laboras21
+{
+    class SuperCanvas : Canvas
+    {
+
+    }
+}
