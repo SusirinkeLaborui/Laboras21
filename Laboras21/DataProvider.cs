ad06d2e3a7c259fb556175ad66df8e6c25bd93eb
AndriusDap <Dapsevicius.Andrius@gmail.com>
2013-10-06 20:32:12 +0300

    Added classes for data generating and reading from file.

---
+++ Laboras21/DataProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras21
+{
+    public static class DataProvider
+    {
+        private DataProvider()
+        {
+        }
+
+        public static List<Point> readFromFile(string filename)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<Point> generate(IRandomNumberGenerator rng)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void saveToFile(string filename, List<Vertex> vertices)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Laboras21/IRandomNumberGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboras21
+{
+    interface IRandomNumberGenerator
+    {
+        public Point getPoint();
+    }
+}
