c9c2d0bd16aed53eb99abdf7836f8638fa1714b2
DariusL <d.lapunas@gmail.com>
2013-10-27 00:22:25 +0300

    Canvas threading/interface probably done

--- Laboras21/SuperCanvas.cs
+++ Laboras21/SuperCanvas.cs
@@ -1,32 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
+using System;
+
+//jo, jo, tuoj
+#pragma warning disable 169
 
 namespace Laboras21
 {
     public class SuperCanvas : Canvas
     {
+        private List<Tuple<Point, Point>> edges = new List<Tuple<Point,Point>>();
+        private IReadOnlyList<Vertex> nodes;
+
         public void SetCollection(IReadOnlyList<Vertex> vertices)
         {
-
-        }
-
-        public void NotifyNodesChanged()
-        {
-
-        }
-
-        public void NotifyNodeChanged(int node)
-        {
-
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                nodes = vertices;
+                Redraw();
+            }));
         }
 
         public void DrawEdge(Vertex vertex1, Vertex vertex2)
         {
+            Dispatcher.BeginInvoke((Action)(() =>
+            {
+                Point p1 = vertex1.Coordinates;
+                Point p2 = vertex2.Coordinates;
+                edges.Add(new Tuple<Point, Point>(p1, p2));
+                DrawEdge(p1, p2);
+            }));
+        }
+
+        private void DrawEdge(Point point1, Point point2)
+        {
+
+        }
+
+        private void Redraw()
+        {
+
         }
     }
 }
