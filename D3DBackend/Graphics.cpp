60f2dff68abb6caac82897dde0a54fae18a815c9
DariusL <d.lapunas@gmail.com>
2013-11-30 19:31:09 +0200

    object files

--- D3DBackend/Graphics.cpp
+++ D3DBackend/Graphics.cpp
@@ -1,10 +1,13 @@
 #include "PrecompiledHeader.h"
 #include "Graphics.h"
-
+#include "ResourceManager.h"
 
 Graphics::Graphics(int windowWidth, int windowHeight, HWND windowHandle) :
-	d3D(windowWidth, windowHeight, windowHandle)
+	d3D(windowWidth, windowHeight, windowHandle),
+	nodes(RM::Get().GetModel(RM::Models::MODEL_CIRCLE), RM::Get().GetShader(), 10000),
+	edges(RM::Get().GetModel(RM::Models::MODEL_SQUARE), RM::Get().GetShader(), 10000)
 {
+	RM::Get().InitShaders(d3D.GetDevice());
 }
 
 Graphics::~Graphics()
@@ -15,7 +18,6 @@ Graphics::~Graphics()
 void Graphics::Render()
 {
 	d3D.StartDrawing(1.0f, 0.0f, 0.0f, 1.0f);
-
 
 	d3D.SwapBuffers();
 }
--- D3DBackend/Graphics.h
+++ D3DBackend/Graphics.h
@@ -3,11 +3,15 @@
 #include "D3D.h"
 #include "Model.h"
 #include "Light.h"
+#include "Instancer.h"
 
 class Graphics
 {
 private:
 	D3D d3D;	
+
+	Instancer nodes;
+	Instancer edges;
 
 public:
 	Graphics(int windowWidth, int windowHeight, HWND windowHandle);
