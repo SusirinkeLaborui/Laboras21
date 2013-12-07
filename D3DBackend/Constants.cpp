f3edd7f60d254fd5ee37e27f7b43696a90f11e4f
Sunius <suniusx@gmail.com>
2013-12-07 20:43:50 +0200

    No need to use higher feature level than 9_1.

--- D3DBackend/Constants.cpp
+++ D3DBackend/Constants.cpp
@@ -17,7 +17,7 @@ const float Constants::ScreenDepth = 500000.0f;
 const float Constants::ScreenNear = 0.1f;
 const float Constants::FieldOfView = 50.625f;
 
-const D3D_FEATURE_LEVEL Constants::D3DFeatureLevel = D3D_FEATURE_LEVEL_10_1;
+const D3D_FEATURE_LEVEL Constants::D3DFeatureLevel = D3D_FEATURE_LEVEL_9_1;
 const D3D11_CULL_MODE Constants::D3DCullMode = D3D11_CULL_BACK;
 const D3D11_FILL_MODE Constants::D3DFillMode = D3D11_FILL_SOLID; // D3D11_FILL_SOLID, D3D11_FILL_WIREFRAME
 
